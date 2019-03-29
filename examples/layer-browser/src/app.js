2335d6e8c998f626a78ab2ae16a13f601ddad0d8
Jian Huang <jianh@uber.com>
2019-03-29 16:55:28 -0700

    integrate phong lighting to mesh layer

--- examples/layer-browser/src/app.js
+++ examples/layer-browser/src/app.js
@@ -19,7 +19,6 @@ import LayerSelector from './components/layer-selector';
 import LayerControls from './components/layer-controls';
 
 import LAYER_CATEGORIES from './examples';
-import '@luma.gl/debug';
 
 /* eslint-disable no-process-env */
 const MapboxAccessToken =
